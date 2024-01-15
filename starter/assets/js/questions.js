025a528706bf3595b571fdc3356e0e34f217374f
James Fryer <james.fryer@live.co.uk>
2024-01-15 12:21:43 +0000

    added .js files and a few examples questions in questions.js

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,24 @@
+var questions = [
+    {
+        title: "What language is typically used in .js files",
+        choices: ['html','css','javascript','python'],
+        answer: 'javascript'
+    },
+    {
+        title: "How many challenges have been completed so far (including this one)",
+        choices: ['3','5','6','10'],
+        answer: '6'
+    },
+    {
+        title: "What does CSS stand for in web development?",
+        choices: ['Computer Style Sheets', 'Cascading Style Sheets', 'Code Style Sheets', 'Creative Style Sheets'],
+        answer: 'Cascading Style Sheets'
+    },
+    {
+        title: "In JavaScript, what method is used to add an element to the end of an array?",
+        choices: ['append()', 'addToEnd()', 'push()', 'addElement()'],
+        answer: 'push()'
+    },
+
+
+]
